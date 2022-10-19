ad68ce125002a1208488704628926acbf10b3bbc
Adam Jermstad <acc@amjerm.me>
2022-10-18 23:03:11 -0500

    feat: extract get_git_log to fn

--- src/lib.rs
+++ src/lib.rs
@@ -7,28 +7,24 @@ pub fn process_repository(repository_path: &str) -> Vec<String> {
     // change the working directory
     let repo_path = Path::new(&repository_path);
     assert!(env::set_current_dir(&repo_path).is_ok());
-    println!(
-        "Successfully changed working directory to {}!",
-        repo_path.display()
-    );
 
-    // get git log for the repository
-    let mut git_log = Command::new("git");
-    git_log
+    let git_log: String = get_git_log();
+    let tickets: Vec<String> = parse_tickets(git_log);
+    tickets
+}
+
+fn get_git_log() -> String {
+    let mut log_command = Command::new("git");
+    log_command
         .arg("log")
         .arg("origin/release")
         .arg("--not")
         .arg("origin/HEAD")
         .arg("--oneline");
 
-    // print the result
-    let log_result: String =
-        String::from_utf8(git_log.output().expect("git log output failed").stdout).unwrap();
-
-    println!("git log result: {}", &log_result);
-
-    let tickets: Vec<String> = parse_tickets(log_result);
-    tickets
+    let command_result = log_command.output().expect("git log output failed").stdout;
+    let log_result: String = String::from_utf8(command_result).unwrap();
+    log_result
 }
 
 fn parse_tickets(string: String) -> Vec<String> {
@@ -38,10 +34,8 @@ fn parse_tickets(string: String) -> Vec<String> {
     assert!(re.is_match("IG-123"));
 
     let ticket_matches = re.find_iter(&string);
-
     let mut tickets: Vec<String> = ticket_matches.map(|x| String::from(x.as_str())).collect();
 
     tickets.dedup();
-
     tickets
 }
